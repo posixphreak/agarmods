df9a28190f0dd28d5ade00c1a9244e1a2b67423b
electronoob <github@electronoob.com>
2015-05-08 13:55:46 -0400

    updates

--- mods.user.js
+++ mods.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         New Agar Server Selector
-// @namespace    
+// @namespac
 // @version      1.1
 // @description  based on Ununoctium118 earlier hack
 // @author       electronoob
@@ -9,6 +9,14 @@
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.src = "http://botb.club/~e/agar.io/mods.js";
+script.src = "https://raw.githubusercontent.com/electronoob/agarmods/master/mods.js";
 (document.body || document.head || document.documentElement).appendChild(script);
 
+/*
+repo:
+https://github.com/electronoob/agarmods
+
+common website for all mods from anybody:
+http://www.agarmods.com
+
+*/
