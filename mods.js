3d824b6a3b0875ca6e9506bed217df012e0b31ee
electronoob <github@electronoob.com>
2015-05-08 19:11:21 -0400

    made the script more informative to simplify hassle

--- mods.js
+++ mods.js
@@ -8,8 +8,19 @@ nodeDiv.id = "includedContent";
 nodeDiv.style.backgroundColor = "#000000";
 nodeDiv.style.zIndex = 9999999999;
 nodeDiv.style.color = "#dddddd";
-nodeDiv.innerHTML = "<small>electronoob's evergreen server script for team play. <a target=\"_blank\" href=\"http://www.agarmods.com/Docs/\">Instructions</a></p>";
+nodeDiv.innerHTML = "<small>electronoob's evergreen server script for team play. <a target=\"_blank\" href=\"http://www.agarmods.com/\">Website</a>.</p>";
 nodeDiv.innerHTML+= "<p>There is a new <a target=\"_blank\" href=\"https://www.reddit.com/r/Agario/comments/3590rk/want_to_team_up_join_the_unofficial_mumble_server/\">Mumble chat here.</a>";
+nodeDiv.innerHTML+= "<u>connections steps</u>";
+nodeDiv.innerHTML+= "\
+<ul>\
+  <li>1: get ip address from friend.</li>\
+  <li>2: put it in text box below.</li>\
+  <li>3: select ANY region.</li>\
+  <li>Everytime you select a region, it will rotate through all servers on the ip address you entered.</li>\
+  <li>There are several worlds per ip address, so check to make sure the leaderboard has same names as your friends one.</li>\
+</ul>\
+\
+";
 
 jQuery('#region').parent().get(0).appendChild( document.createElement("br"));
 jQuery('#region').parent().get(0).appendChild(nodeDiv);
@@ -27,6 +38,7 @@ jQuery( playBtn ).parent().get( 0 ).appendChild(nodeBr);
 jQuery( playBtn ).parent().get( 0 ).appendChild(nodeInput);
 
 jQuery('#iphack').change(function () {
+if (jQuery('#iphack').val() == "") {modBlocking = true;}
 modBlocking = false;
 });
 
@@ -47,8 +59,12 @@ jQuery('#region').change(function () {
             },
             success: function(a) {
                 a = a.split("\n");
-                jQuery('#includedContent').html(a[0]);
-            },
+		if (modBlocking == true ) {
+	                jQuery('#includedContent').html("Here is the IP address of the server you are connected to currently, pass it to your friends for team playing. <h3>" + a[0] + "</h3>");
+          	} else {
+			jQuery('#includedContent').html("<h3>Connecting to " +  jQuery('#iphack').val() + "</h3><br>Check leaderboard with your friend to ensure you are both on the exact world on the same server.<br><br>If you cannot see the same people in the leaderboard as your friend, randomly select a different region and it will try another world on the same server.");
+		}
+	    },
             dataType: "text",
             method: "POST",
             cache: !1,
