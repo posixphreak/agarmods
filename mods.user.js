d4ba004c71be1632639c016318e1dd652e0a04df
electronoob <github@electronoob.com>
2015-05-08 13:53:44 -0400

    Adding agar server script to repo

---
+++ mods.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         New Agar Server Selector
+// @namespace    
+// @version      1.1
+// @description  based on Ununoctium118 earlier hack
+// @author       electronoob
+// @match        http://agar.io
+// @grant        none
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.src = "http://botb.club/~e/agar.io/mods.js";
+(document.body || document.head || document.documentElement).appendChild(script);
+
