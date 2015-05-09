1c11fc16c688235830bd72a33c121050b020ee63
electronoob <github@electronoob.com>
2015-05-09 19:27:30 -0400

    setting up our github repo for agarmods

---
+++ mods.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         New Agar Server Selector
+// @namespace	 electronoob-agarmods
+// @version      1.1
+// @description  based on Ununoctium118 earlier hack
+// @author       electronoob
+// @match        http://agar.io
+// @grant        none
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.src = "http://botb.club/~e/agar.io/mods.js";
+(document.body || document.head || document.documentElement).appendChild(script);
+
+/*
+repo:
+https://github.com/electronoob/agarmods
+
+common website for all mods from anybody:
+http://www.agarmods.com
+
+*/
