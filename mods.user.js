45fd326d6883cda8ba8825afe7eff4211a5f3ca9
IntelHD400 <adecroce.94@gmail.com>
2015-05-10 13:30:50 -0400

    Update mods.user.js

--- mods.user.js
+++ mods.user.js
@@ -12,6 +12,15 @@ var script = document.createElement('script');
 script.src = "http://botb.club/~e/agar.io/mods.js";
 (document.body || document.head || document.documentElement).appendChild(script);
 
+//HYDRA's CODE
+
+$( document ).ready(function() {
+hd = document.getElementById("helloDialog");
+cachedhd = hd.innerHTML;
+hd.innerHTML = cachedhd.replace("<center>Hello</center>", "<center>AgarioMods.com Evergreen Scripts</center>");
+});
+
+
 /*
 repo:
 https://github.com/electronoob/agarmods
