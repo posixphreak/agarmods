91ac7abfc7fd949a37e2964de495d560cc762192
IntelHD400 <adecroce.94@gmail.com>
2015-05-10 13:48:29 -0400

    Update mods.user.js

--- mods.user.js
+++ mods.user.js
@@ -9,7 +9,7 @@
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.src = "http://botb.club/~e/agar.io/mods.js";
+script.src = "http://agariomods.com/mods.js";
 (document.body || document.head || document.documentElement).appendChild(script);
 
 /*
