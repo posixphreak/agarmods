9db5ac87b58d8cbbd34a39e57776c1dfa9ff11c6
electronoob <github@electronoob.com>
2015-05-12 00:06:02 +0200

    pretty me

---
+++ mods.1.4.1.hax.js
@@ -0,0 +1,38 @@
+var tester = document.getElementsByTagName("script");
+
+var i = 0, main_out_url = "http://agar.io/main_out.js", discovered_mainouturl = 0;
+for (i=0; i<tester.length; i++ ){
+	//alert(tester[i].src);
+	src = tester[i].src;
+	if (src.substring(0, main_out_url.length ) == main_out_url) {
+		discovered_mainouturl = src.replace("http://agar.io/","");
+	}
+}
+
+var gamejs = "";
+if(discovered_mainouturl != !1) {
+	// we have found the main game js file, sweet.
+	$.ajax({
+  	url: discovered_mainouturl,
+ 	 	success:function(data){
+			gamejs = "window.agariomods = " + data.replace("socket open","intercepted game js socket open");
+			agariomodsRuntimeInjection();
+  		}
+	});
+}
+
+function agariomodsRuntimeInjection() {
+	var tester = document.getElementsByTagName("html");
+	var oldhtml = tester[0].innerHTML;
+	var script = document.createElement("script");
+	agariomodsRuntimePatches();
+	script.innerHTML = gamejs;
+	document.head.appendChild(script);
+}
+
+function agariomodsRuntimePatches() {
+	gamejs = gamejs.replace(';reddit;',';reddit;electronoob;');
+	gamejs = gamejs.replace('W[b]=new Image,W[b].src="skins/"+b+".png"','W[b]=new Image,W[b].crossOrigin = "Anonymous",W[b].src="skins/"+b+".png"');
+	gamejs = gamejs.replace('b=this.name.toLowerCase();', 'b=this.name.toLowerCase();var agariomods="";if(b == "electronoob") {agariomods="http://agariomods.com/skins/electronoob";} else {agariomods="http://agar.io/skins/" + this.name.toLowerCase();}');
+	gamejs = gamejs.replace('W[b].src="skins/"+b+".png"','W[b].src=agariomods+".png"');
+}
