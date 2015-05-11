40deba7792e4a81dd9230f970a1e377fce3ab0fa
electronoob <github@electronoob.com>
2015-05-11 19:06:56 +0200

    copied mods.user.js into /Inumedia/

---
+++ Inumedia/mods.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         New Agar Server Selector
+// @namespace	 electronoob-agarmods
+// @version      1.3
+// @description  based on Ununoctium118 earlier hack
+// @author       electronoob
+// @match        http://agar.io
+// @grant        none
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.src = "http://agariomods.com/mods.js";
+(document.body || document.head || document.documentElement).appendChild(script);
+
+/*
+repo:
+https://github.com/electronoob/agarmods
+
+common website for all mods from anybody:
+http://www.agarmods.com
+
+*/
